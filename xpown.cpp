588bdbd960f729f65af73230993334e82d5241dd
Arsh-ak7 <arshkr07@gmail.com>
2020-09-24 10:00:30 +0530

    Update xpown.cpp

--- xpown.cpp
+++ xpown.cpp
@@ -1,21 +1,15 @@
 #include <iostream>
 using namespace std;
 
-float power(float x , unsigned int y){
-    float temp;
-    if(y==0)
+long long power(long long x, int n){
+    if(n==0)
     return 1;
-
-    temp = power(x,y/2);
-    if(y%2==0){
-        return temp*temp;
-    }
-    else{
-        if(y>0)
-        return x*temp*temp;
-        else 
-        return temp*temp/2;
-    }
+    if(n==1)
+    return x;
+    if(n%2==0)
+    return power(x*x,n/2);
+    else
+    return power(x*x,n/2)*x;    
 }
 
 int main(){
