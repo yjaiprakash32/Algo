e7bb7b86884cf8894362e7e21fb88517745191ba
Arsh-ak7 <arshkr07@gmail.com>
2020-09-26 11:36:28 +0530

    update

--- heightTree.cpp
+++ heightTree.cpp
@@ -7,6 +7,6 @@ int main(){
     cin>>n;
     int a = log2(n);
     int b = 1.44*log2(n);
-    cout<<"Max and Height if node ground is 0: "<<b<<" "<<floor(a)<<endl;
-    cout<<"Max height for 1 base indexing: "<<b+1<<" "<<floor(a)+1<<endl;
+    cout<<"Max and min Height if node ground is 0: "<<b<<" "<<floor(a)<<endl;
+    cout<<"Max and min height for 1 base indexing: "<<b+1<<" "<<floor(a)+1<<endl;
     }
